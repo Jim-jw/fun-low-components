ffcd097810e56b2f59cf1bd64bc714532f56e06f
wujie <wujie_mvp@163.com>
2020-12-16 17:39:27 +0800

    feat: link

---
+++ packages/FlLink/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-link
+    v-bind="$attrs"
+    :underline="underline"
+    :target="target">
+    <slot/>
+  </el-link>
+</template>
+
+<script>
+import { Link } from 'element-ui';
+
+export default {
+  name: 'FlLink',
+  components: {
+    ElLink: Link,
+  },
+  props: {
+    underline: {
+      type: Boolean,
+      default: false,
+    },
+    target: {
+      type: String,
+      default: '_blank',
+    },
+  },
+};
+</script>
